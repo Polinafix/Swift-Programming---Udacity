78a3453d51afe40bc6ae6a49de1a0cc32ad124fe
polinafiksson <pd.fiksson@gmail.com>
2017-01-19 13:56:57 -0600

    solved exercises for lesson2-optional type

--- L2_Exercises.playground/Contents.swift
+++ L2_Exercises.playground/Contents.swift
@@ -6,35 +6,56 @@ import UIKit
 //: When retrieving a value from a dictionary, Swift returns an optional.
 //:
 //: 1a) The variable, director, is an optional type. Its value cannot be used until it is unwrapped. Use `if let` to carefully unwrap the value returned by `moviesDict[movie]`
+//The values of a dictionary can be accessed through subscripting by specifying the corresponding key in square brackets: someDictionary["Alex"] refers to the value associated with the key "Alex". The subscript returns an optional value of the dictionary’s value type. If the specified key isn’t contained in the dictionary, the subscript returns nil.
+
 var moviesDict:Dictionary = [ "Star Wars":"George Lucas", "Point Break":"Kathryn Bigelow"]
 var movie = "Point Break"
+//var movie = "Point B"
 var director = moviesDict[movie]
+type(of: director)
+//My Solution
+if let theDirector = director{
+    print("\(theDirector) directed \(movie)")
+}else{
+    print("No such movie exists")
+}
 
 //: 1b) Test your code by inserting different values for the variable `movie`.
 
 //: ### Exercise 2
 //: The LoginViewController class below needs a UITextField to hold a user's name. Declare a variable for this text field as a property of the class LoginViewController. Keep in mind that the textfield property will not be initialized until after the view controller is constructed.
 class LoginViewController: UIViewController {
+    //My Solution
+    var name: UITextField!
 }
 
 //: ### Exercise 3
 //: The Swift Int type has an initializer that takes a string as a parameter and returns an optional of type Int?.
 //:
 //: 3a) Finish the code below by safely unwrapping the constant, `number`.
+//var numericalString = "three"
 var numericalString = "3"
-var number = Int(numericalString)
+var number = Int(numericalString)//returns an optional Int
+//type(of:number)
 //TODO: Unwrap number to make the following print statement more readable.
-print("\(number) is the magic number.")
+//My Solution
+if let number = Int(numericalString){
+    print("\(number) is the magic number.")
+    
+}else{
+    print("there is an error")
+}
 //: 3b) Change the value of numericalString to "three" and execute the playground again.
 
 //: ### Exercise 4
 //: The class UIViewController has a property called tabBarController.  The tabBarController property is an optional of type UITabBarController?. The tabBarController itself holds a tabBar as a property. Complete the code below by filling in the appropriate use of optional chaining to access the tab bar property.
 var viewController = UIViewController()
-//if let tabBar = //TODO: Optional chaining here {
-//    print("Here's the tab bar.")
-//} else {
-//    print("No tab bar controller found.")
-//}
+//My Solution
+if let tabBar = viewController.tabBarController?.tabBar{
+    print("Here's the tab bar.")
+} else {
+    print("No tab bar controller found.")
+}
 //: ### Exercise 5
 //: Below is a dictionary of paintings and artists.
 //:
@@ -42,7 +63,15 @@ var viewController = UIViewController()
 
 var paintingDict:Dictionary = [ "Guernica":"Picasso", "Mona Lisa": "da Vinci", "No. 5": "Pollock"]
 var painting = "Mona Lisa"
+//var painting = "Mona Li"
 var artist = paintingDict[painting]
+
+//My Solution
+if let artist = artist {
+    print("\(artist) created \(painting)")
+}else{
+    print("There is no such artist")
+}
 
 //: 5b) Test your code by inserting different values for the variable `painting`.
 
@@ -52,7 +81,16 @@ var anotherViewController = UIViewController()
 var cancelButton: UIBarButtonItem!
 cancelButton = UIBarButtonItem()
 // TODO: Set the width of the cancel button.
+//My Solution
+// Properties of implicitly unwrapped optionals are set like any old property, because implicitly unwrapped optionals unwrap automatically.
+cancelButton.width = 15
 //: ### Exercise 7
 //: The class UIViewController has a property called parentViewController.  The parentViewController property is an optional of type UIViewController?. We can't always be sure that a given view controller has a parentViewController.  Safely unwrap the parentViewController property below using if let.
 var childViewController = UIViewController()
 // TODO: Safely unwrap childViewController.parentViewController
+// My Solution
+if let parentVC = childViewController.parent {
+    print("Here's the parentViewController")
+} else {
+    print("There is no parent")
+}
